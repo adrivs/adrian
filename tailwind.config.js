3a16d538d7df41aad995cd8ef44466425b894fff
adrivs <adrianvillalbasanchez@gmail.com>
2024-02-05 19:05:21 +0100

    Start project and set pages

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'silver-chalice': {
+					50: '#f7f7f7',
+					100: '#ededed',
+					200: '#dfdfdf',
+					300: '#c8c8c8',
+					400: '#aaaaaa',
+					500: '#999999',
+					600: '#888888',
+					700: '#7b7b7b',
+					800: '#676767',
+					900: '#545454',
+					950: '#363636'
+				}
+			}
+		}
+	},
+	plugins: []
+}
